6fde24895e68cd81185f3536d3c67c8f90da2b38
maic <maic.ma@outlook.com>
2022-02-25 10:12:37 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { Transmission } from './someClass';
+import {ref} from 'vue'
+
+const instance = ref<Transmission | null>(null)
+
+instance.value = new Transmission({url: '', password: '', username: '', platformType: 'web'})
+
+</script>
+
+<template>
+  <HelloWorld :instance="instance" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
