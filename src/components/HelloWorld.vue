74e08f5f8d75f939b99e99a01c6e0f0952fb7d22
maic <maic.ma@outlook.com>
2022-02-25 10:11:54 +0800

    Create HelloWorld.vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Transmission } from '../someClass'
+
+defineProps<{ instance: Transmission | null }>()
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
